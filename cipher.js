a12dc5c7ddd2d47c2852c2eb3b8adc52d8674564
Marione-Pereira <mari.tainara@gmail.com>
2022-06-16 18:05:10 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -4,7 +4,6 @@ const cipher = {
       throw new TypeError();
     }
 
-
     let mgsCode = "";
 
     for (let i = 0; i < string.length; i++) {
@@ -12,32 +11,14 @@ const cipher = {
 
       if (encrypto >= 65 && encrypto <= 90) {
         mgsCode += String.fromCharCode(((encrypto - 65 + offset) % 26) + 65);
-
       } else if (encrypto >= 97 && encrypto <= 122) {
         mgsCode += String.fromCharCode(((encrypto - 97 + offset) % 26) + 97);
-
-      } else if (encrypto >= 33 && encrypto <= 47) {
+      } else {
         mgsCode += string.charAt(i);
-
-      } else if (encrypto >= 58 && encrypto <= 64) {
-        mgsCode += string.charAt(i);
-
-      } else if (encrypto >= 91 && encrypto <= 96) {
-        mgsCode += string.charAt(i);
-
-      } else if (encrypto >= 123 && encrypto <= 254) {
-        mgsCode += string.charAt(i);
-
-      } else if (encrypto == 32) {
-        mgsCode += string.charAt(i);
-
       }
     }
-
-
-    return mgsCode; //resultado 
+    return mgsCode;
   },
-
 
   decode: function (offset, string) {
     if (offset == 0 || offset == null || offset == undefined) {
@@ -50,26 +31,15 @@ const cipher = {
 
       if (decrypto >= 65 && decrypto <= 90) {
         mgsDecode += String.fromCharCode(((decrypto + 65 - offset) % 26) + 65);
-
       } else if (decrypto >= 97 && decrypto <= 122) {
-        mgsDecode += String.fromCharCode(((decrypto - 122 - offset) % 26) + 122);
-
-      } else if (decrypto >= 33 && decrypto <= 47) {
+        mgsDecode += String.fromCharCode(
+          ((decrypto - 122 - offset) % 26) + 122
+        );
+      } else {
         mgsDecode += string.charAt(i);
-
-      } else if (decrypto >= 58 && decrypto <= 64) {
-        mgsDecode += string.charAt(i);
-      } else if (decrypto >= 91 && decrypto <= 96) {
-        mgsDecode += string.charAt(i);
-      } else if (decrypto >= 123 && decrypto <= 254) {
-        mgsDecode += string.charAt(i);
-
-      } else if (decrypto == 32) {
-        mgsDecode += string.charAt(i);
-
       }
     }
     return mgsDecode;
-  }
+  },
 };
 export default cipher;
--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 import cipher from "./cipher.js";
-const btnCode = document.getElementById("btnCodificar");
-btnCode.addEventListener("click", encryptText)
+const btnCode = document.getElementById("cipher-button");
+btnCode.addEventListener("click", encryptText);
 
 function encryptText(e) {
-  e.preventDefault()
+  e.preventDefault();
   let offset = Number(document.getElementById("offset").value);
-  let string = document.getElementById("codificarTexto").value;
+  let string = document.getElementById("original-text").value;
   const encryptando = cipher.encode(offset, string);
-  document.getElementById("textcode").value = encryptando;
-
+  document.getElementById("returned-text").value = encryptando;
 }
-const btnDeco = document.getElementById("btnDecriptar");
-btnDeco.addEventListener("click", decryptTexto)
+const btnDeco = document.getElementById("decipher-button");
+btnDeco.addEventListener("click", decryptTexto);
 
 function decryptTexto(e) {
-  e.preventDefault()
+  e.preventDefault();
 
   let offset = Number(document.getElementById("offset").value);
-  let string = document.getElementById("decoficarTexto").value;
+  let string = document.getElementById("original-text").value;
   const decryptando = cipher.decode(offset, string);
-  document.getElementById("textdecode").value = decryptando;
+  document.getElementById("returned-text").value = decryptando;
+}
 
+document.getElementById("limpar").addEventListener("click", limpar);
+
+function limpar() {
+  document.getElementById("original-text").value = "";
+  document.getElementById("returned-text").value = "";
+  document.getElementById("offset").value = "1";
 }
 
 
